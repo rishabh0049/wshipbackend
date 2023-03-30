620619448e9a11f8000ba76feadc573b5ea024d4
Rishabh Saini <rishabhsaini049@gmail.com>
2023-03-30 16:30:50 +0530

    msg

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-const express = require('express')
-const app = express()
-const AWS = require('aws-sdk')
-const { v4: uuid } = require('uuid')
-const cors = require('cors')
-const region = 'us-east-1'
+const express = require("express");
+const app = express();
+const AWS = require("aws-sdk");
+const { v4: uuid } = require("uuid");
+const cors = require("cors");
+const region = "us-east-1";
 
-const chime = new AWS.Chime({ region })
-chime.endpoint = new AWS.Endpoint(
-    'https://service.chime.aws.amazon.com/'
-)
-let meetingResponse,attendeeResponse;
-app.get('/meeting', cors(), async (req, res) => {
-    try {
-        
-        meetingResponse = await chime
-            .createMeeting({
-                ClientRequestToken: uuid(),
-                MediaRegion: region,
-            })
-            .promise()
-      
-       
-        console.log(meetingResponse)
-    res.send(meetingResponse)
-    } catch (err) {
-        res.send(err)
-    }
-})
-app.get('/', cors(), async (req, res) => {
+const chime = new AWS.Chime({ region });
+chime.endpoint = new AWS.Endpoint("https://service.chime.aws.amazon.com/");
+
+app.get("/meeting", cors(), async (req, res) => {
+  try {
+    const meetingResponse = await chime
+      .createMeeting({
+        ClientRequestToken: uuid(),
+        MediaRegion: region,
+      })
+      .promise();
+
+    console.log(meetingResponse);
+    res.send(meetingResponse);
+  } catch (err) {
+    res.send(err);
+  }
+});
+app.get("/", cors(), async (req, res) => {
+  res.send("Home");
+});
+app.get("/addattendee", cors(), async (req, res) => {
+  try {
+    console.log(req.query.id)
     
-    res.send("Home")
-   
-})
-app.get('/addattendee', cors(), async (req, res) => {
-    try {
-      console.log(meetingResponse.Meeting.MeetingId,)
-        attendeeResponse = await chime
-        .createAttendee({
-            MeetingId:meetingResponse.Meeting.MeetingId,
-            ExternalUserId: uuid(),
-        })
-        .promise()
-        console.log(attendeeResponse)
-    res.send(attendeeResponse)
-    } catch (err) {
-        res.send(err)
-    }
-})
-app.listen(5002, () => console.log(`Video calling server listening at http://localhost:5000`))
+    const attendeeResponse = await chime
+      .createAttendee({
+        MeetingId:req.query.id,
+        ExternalUserId: uuid(),
+      })
+      .promise();
+    console.log(attendeeResponse);
+    res.send(attendeeResponse);
+  } catch (err) {
+    console.log(err)
+    res.send(err);
+  }
+});
+
+app.listen(5002, () =>
+  console.log(`Video calling server listening at http://localhost:5002`)
+);
